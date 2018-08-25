6428e4f9bdb6c9909ce25131ec445814d7cd9498
hrbrmstr <bob@rud.is>
2018-08-25 16:54:27 -0400

    R package repo initialization complete

---
+++ R/worldtilegrid-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/worldtilegrid>
+#' - BugReports: <https://gitlab.com/hrbrmstr/worldtilegrid/issues>
+#' 
+#' @md
+#' @name worldtilegrid
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
